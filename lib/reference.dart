ab6a2a925ef332876ddff280de0ce6943558a18a
Radhika Joshi <radhika7002@gmail.com>
2024-04-17 00:27:32 -0700

    update FileImage to AssetImage & fix the firebase path.

--- lib/reference.dart
+++ lib/reference.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:io';
 
 class IPadPro1291 extends StatelessWidget {
   const IPadPro1291({super.key});
@@ -68,11 +69,16 @@ class IPadPro1291 extends StatelessWidget {
                     Positioned(
                       left: 141,
                       top: 745,
-                      child: Image.network(
-                        'https://firebasestorage.googleapis.com/v0/b/codeless-app.appspot.com/o/projects%2FVLycwtx8L8la6K4YYSH8%2F4b1eef2377258f507f56a630b90796174f92cdafRectangle%206.png?alt=media&token=e35470dd-b23c-4f8b-b2fb-9d5b5f2cecc4',
+                      child: Container(
                         width: 266,
                         height: 229,
-                        fit: BoxFit.cover,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/firebaseImages/dices2Image.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
                     ),
                     Positioned(
@@ -114,31 +120,46 @@ class IPadPro1291 extends StatelessWidget {
                     Positioned(
                       left: 618,
                       top: 1102,
-                      child: Image.network(
-                        'https://firebasestorage.googleapis.com/v0/b/codeless-app.appspot.com/o/projects%2FVLycwtx8L8la6K4YYSH8%2Ffd88a8ac45408c734505345100218c41742b25daballs%20blurr%201.png?alt=media&token=274b1f57-0706-490f-9c59-f6600c36923c',
+                      child: Container(
                         width: 225,
                         height: 225,
-                        fit: BoxFit.cover,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/firebaseImages/trainglecolorballs.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
                     ),
                     Positioned(
                       left: 623,
                       top: 743,
-                      child: Image.network(
-                        'https://firebasestorage.googleapis.com/v0/b/codeless-app.appspot.com/o/projects%2FVLycwtx8L8la6K4YYSH8%2Fbc6a8b33461c1f15a34e3a03a34ed7d5470d118fwheel%20b.png?alt=media&token=cf68b70c-b2b8-4ad8-a47d-e9171e06eb66',
+                      child: Container(
                         width: 228,
                         height: 224,
-                        fit: BoxFit.cover,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/firebaseImages/colorwheel.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
                     ),
                     Positioned(
                       left: 146,
                       top: 1101,
-                      child: Image.network(
-                        'https://firebasestorage.googleapis.com/v0/b/codeless-app.appspot.com/o/projects%2FVLycwtx8L8la6K4YYSH8%2Fea4ccdc8b18ef59430ab534e835eb2bf6293b247head%20and%20tails%20%20blur.png?alt=media&token=b66a2b8a-9329-4d7e-9e74-3e478cb2f111',
+                      child: Container(
                         width: 255,
                         height: 200,
-                        fit: BoxFit.cover,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/firebaseImages/headAndTails.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
                     ),
                     Positioned(
@@ -193,11 +214,16 @@ class IPadPro1291 extends StatelessWidget {
                     Positioned(
                       left: 509,
                       top: 632,
-                      child: Image.network(
-                        'https://storage.googleapis.com/codeless-dev.appspot.com/uploads%2Fimages%2FVLycwtx8L8la6K4YYSH8%2F7db092c70113da2a1e937969d5c5b1d1.png',
+                      child: Container(
                         width: 400,
                         height: 4,
-                        fit: BoxFit.contain,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/firebaseImages/horizontalWhiteLineThick.png"),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
                       ),
                     ),
                     Positioned(
@@ -328,11 +354,16 @@ class IPadPro1291 extends StatelessWidget {
                             Positioned(
                               left: 13,
                               top: 10,
-                              child: Image.network(
-                                'https://storage.googleapis.com/codeless-dev.appspot.com/uploads%2Fimages%2FVLycwtx8L8la6K4YYSH8%2F8e41733af163909d6944dac1c628ba81.png',
+                              child: Container(
                                 width: 53,
                                 height: 60,
-                                fit: BoxFit.contain,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        "assets/images/firebaseImages/homeba81.png"),
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
                               ),
                             )
                           ],
@@ -342,21 +373,31 @@ class IPadPro1291 extends StatelessWidget {
                     Positioned(
                       left: 25,
                       top: 1294,
-                      child: Image.network(
-                        'https://storage.googleapis.com/codeless-dev.appspot.com/uploads%2Fimages%2FVLycwtx8L8la6K4YYSH8%2Fbf1e1e2ac7f3a97b01703e3215b9a910.png',
+                      child: Container(
                         width: 62,
                         height: 59,
-                        fit: BoxFit.contain,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/firebaseImages/backbeaf5.png"),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
                       ),
                     ),
                     Positioned(
                       left: 961,
                       top: 1294,
-                      child: Image.network(
-                        'https://storage.googleapis.com/codeless-dev.appspot.com/uploads%2Fimages%2FVLycwtx8L8la6K4YYSH8%2Fbaae1378b31042b9a1c6751db15bf804.png',
+                      child: Container(
                         width: 63,
                         height: 63,
-                        fit: BoxFit.contain,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/firebaseImages/home804.png"),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
                       ),
                     ),
                     Positioned(
